3f6810f4142f1fcea77f4934a39b8d9b100ec985
thayronf3 <thayronf3@gmail.com>
2020-05-24 20:51:58 -0300

    Foi criado um arquivo de faz a busca de todos os alunos que estão no banco de dados usando o metoso findAll()

---
+++ comands/BuscarAlunos.php
@@ -0,0 +1,17 @@
+<?php
+
+use Alura\Doctrine\Entity\Aluno;
+use Alura\Doctrine\Helper\EntityManagerFactory;
+
+require_once __DIR__. '/../vendor/autoload.php';
+
+$entityManagerFactory = new EntityManagerFactory();
+$entityManager = $entityManagerFactory->getEntityManager();
+
+$alunoRepository = $entityManager->getRepository(Aluno::class);
+
+$alunoList = $alunoRepository->findAll();
+
+foreach($alunoList as $aluno){
+    echo"ID: {$aluno->getId()} \nNome: {$aluno->getNome()}\n\n"; 
+}
